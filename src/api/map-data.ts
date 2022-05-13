0fdc8a6cbc623640b7054dfb7245e2dc1f97c03c
gustavobgt <gustavo_bgt97@hotmail.com>
2022-05-13 11:57:28 -0300

    f2

--- src/api/map-data.ts
+++ src/api/map-data.ts
@@ -1,26 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PageData } from '../templates/Home';
 import { mapMenu } from './map-menu';
-import { mapSections } from './map-sections';
 
 export const mapData = (pagesData = [{}] as any): PageData[] => {
-  return pagesData.map(
-    (data: any): PageData => {
-      const {
-        footer_text: footerHtml = '',
-        slug = '',
-        title = '',
-        sections = [],
-        menu = {},
-      } = data;
+  return pagesData.map((data: any): PageData => {
+    const {
+      footer_text: footerHtml = '',
+      slug = '',
+      title = '',
+      sections = [],
+      menu = {},
+    } = data;
 
-      return {
-        footerHtml,
-        slug,
-        title,
-        sections: mapSections(sections),
-        menu: mapMenu(menu),
-      };
-    },
-  );
+    return {
+      footerHtml,
+      slug,
+      title,
+      menu: mapMenu(menu),
+    };
+  });
 };
